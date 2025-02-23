e89465cdb820fe7a54457bc669321a574be25478
iyiu9afir <iyiu9afir@gmail.com>
2025-02-23 18:15:00 +0600

    overall checked

--- multiplication with loop/script.js
+++ multiplication with loop/script.js
@@ -1,11 +1,8 @@
 function multiply() {
-  var getValue1 = document.getElementById("number1").value;
-  var getValue2 = document.getElementById("number2").value;
-  var showResult1 = document.getElementById("results");
-  // var showResult2 = document.getElementById("result2");
-  var i;
-  var text;
-  showResult1.innerHTML = "";
+  let getValue1 = Number(document.getElementById("number1").value);
+  let getValue2 = Number(document.getElementById("number2").value);
+  let showResult = document.getElementById("results");
+  showResult.innerHTML = "";
   if (getValue1 > getValue2) {
     [getValue1, getValue2] = [getValue2, getValue1];
   }
@@ -17,20 +14,18 @@ function multiply() {
     getValue2 < 1 ||
     getValue2 > 20
   ) {
-    text = "Not Valid!";
+    let text = "Not Valid!";
     document.getElementById("warning").innerHTML = text;
     document.getElementById("success").innerHTML = "";
   } else {
     text = "Proceed";
-    for (i = getValue1; i <= getValue2; i++) {
-      // showResult1.innerHTML += `${getValue1} x ${i} = ${getValue1 * i} <br/>`;
-      // showResult2.innerHTML += `${getValue2} x ${i} = ${getValue2 * i} <br/>`;
+    for (let i = getValue1; i <= getValue2; i++) {
       let tableHTML = `<div class="table-part">`;
       for (let j = 1; j <= 10; j++) {
         tableHTML += `${i} x ${j} = ${i * j} <br/>`;
       }
       tableHTML += "</div>";
-      showResult1.innerHTML += tableHTML;
+      showResult.innerHTML += tableHTML;
     }
     document.getElementById("warning").innerHTML = "";
     document.getElementById("success").innerHTML = text;
