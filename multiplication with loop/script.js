40f001b41ca97679cfe20bbffd7194af1312d10e
iyiu9afir <iyiu9afir@gmail.com>
2025-02-23 16:25:20 +0600

    multiple table solved with from digit to digit condition

--- multiplication with loop/script.js
+++ multiplication with loop/script.js
@@ -3,10 +3,14 @@
 function multiply() {
   var getValue1 = document.getElementById("number1").value;
   var getValue2 = document.getElementById("number2").value;
-  var showResult1 = document.getElementById("result1");
-  var showResult2 = document.getElementById("result2");
+  var showResult1 = document.getElementById("results");
+  // var showResult2 = document.getElementById("result2");
   var i;
   var text;
+  showResult1.innerHTML="";
+  if(getValue1>getValue2){
+    [getValue1,getValue2]=[getValue2,getValue1];
+  }
   if (
     isNaN(getValue1) ||
     getValue1 < 1 ||
@@ -18,9 +22,15 @@ function multiply() {
     text = "not valid";
   } else {
     text = "output";
-    for (i = 1; i <= 10; i++) {
-      showResult1.innerHTML += `${getValue1} x ${i} = ${getValue1 * i} <br/>`;
-      showResult2.innerHTML += `${getValue2} x ${i} = ${getValue2 * i} <br/>`;
+    for (i = getValue1; i <= getValue2; i++) {
+      // showResult1.innerHTML += `${getValue1} x ${i} = ${getValue1 * i} <br/>`;
+      // showResult2.innerHTML += `${getValue2} x ${i} = ${getValue2 * i} <br/>`;
+      let tableHTML=`<div class="table-part">`
+      for(let j=1; j<=10; j++){
+        tableHTML+= `${i} x ${j} = ${i*j} <br/>`
+      }
+      tableHTML+="</div>";
+      showResult1.innerHTML+=tableHTML;
     }
   }
   document.getElementById("warning").innerHTML = text;
