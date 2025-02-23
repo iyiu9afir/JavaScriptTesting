0daeabdab5b979b83f3ac94fa05624dca2b13fed
iyiu9afir <iyiu9afir@gmail.com>
2025-02-23 16:59:49 +0600

    color modified

--- multiplication with loop/script.js
+++ multiplication with loop/script.js
@@ -1,5 +1,3 @@
-
-
 function multiply() {
   var getValue1 = document.getElementById("number1").value;
   var getValue2 = document.getElementById("number2").value;
@@ -7,9 +5,9 @@ function multiply() {
   // var showResult2 = document.getElementById("result2");
   var i;
   var text;
-  showResult1.innerHTML="";
-  if(getValue1>getValue2){
-    [getValue1,getValue2]=[getValue2,getValue1];
+  showResult1.innerHTML = "";
+  if (getValue1 > getValue2) {
+    [getValue1, getValue2] = [getValue2, getValue1];
   }
   if (
     isNaN(getValue1) ||
@@ -19,21 +17,22 @@ function multiply() {
     getValue2 < 1 ||
     getValue2 > 20
   ) {
-    text = "not valid";
+    text = "Not Valid!";
+    document.getElementById("warning").innerHTML = text;
+    document.getElementById("success").innerHTML = "";
   } else {
-    text = "output";
+    text = "Proceed";
     for (i = getValue1; i <= getValue2; i++) {
       // showResult1.innerHTML += `${getValue1} x ${i} = ${getValue1 * i} <br/>`;
       // showResult2.innerHTML += `${getValue2} x ${i} = ${getValue2 * i} <br/>`;
-      let tableHTML=`<div class="table-part">`
-      for(let j=1; j<=10; j++){
-        tableHTML+= `${i} x ${j} = ${i*j} <br/>`
+      let tableHTML = `<div class="table-part">`;
+      for (let j = 1; j <= 10; j++) {
+        tableHTML += `${i} x ${j} = ${i * j} <br/>`;
       }
-      tableHTML+="</div>";
-      showResult1.innerHTML+=tableHTML;
+      tableHTML += "</div>";
+      showResult1.innerHTML += tableHTML;
     }
+    document.getElementById("warning").innerHTML = "";
+    document.getElementById("success").innerHTML = text;
   }
-  document.getElementById("warning").innerHTML = text;
 }
-
-
