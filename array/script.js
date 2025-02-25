34f0949e5e88f6e315819d6e55ed0e9cbcba4172
iyiu9akhan <iyiu9akhan@gmail.com>
2025-02-25 22:56:33 +0600

    array -> pop / push / shift / unshift

---
+++ array/script.js
@@ -0,0 +1,24 @@
+let demo_one = ["demo one", "demo two", "demo three", "demo four"];
+
+demo_one.shift()
+console.log(demo_one);
+
+
+let demo_four = ["demo one", "demo two", "demo three", "demo four"];
+
+demo_four.pop()
+console.log(demo_four);
+
+
+let demo_two = ["demo one", "demo two", "demo three", "demo four"];
+
+demo_two.unshift("unshift")
+console.log(demo_two);
+
+
+
+let demo_three = ["demo one", "demo two", "demo three", "demo four"];
+
+demo_three.push("push")
+console.log(demo_three);
+
